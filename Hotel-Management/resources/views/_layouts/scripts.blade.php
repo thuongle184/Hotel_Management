48851cd1c756be262e0d89aad2b5cc73578b3f71
tuan.nguyen1 <tuan.nguyen.106902@gmail.com>
2019-05-20 11:43:17 +0700

    Finished identificationType

--- Hotel-Management/resources/views/_layouts/scripts.blade.php
+++ Hotel-Management/resources/views/_layouts/scripts.blade.php
@@ -1,7 +1,8 @@
-<script src="{{URL::asset('js/jquery.min.js')}}"></script>
-<script src="{{URL::asset('jquery-ui/jquery-ui.min.js')}}"></script>
-<script src="{{URL::asset('bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{URL::asset('js/filter.js')}}"></script>
-<script src="{{URL::asset('js/entities/country.js')}}"></script>
-<script src="{{URL::asset('js/entities/dish.js')}}"></script>
-<script src="{{URL::asset('js/entities/dishType.js')}}"></script>
+<script src="{{URL::asset('public/js/jquery.min.js')}}"></script>
+<script src="{{URL::asset('public/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{URL::asset('public/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{URL::asset('public/js/filter.js')}}"></script>
+<script src="{{URL::asset('public/js/entities/country.js')}}"></script>
+<script src="{{URL::asset('public/js/entities/dish.js')}}"></script>
+<script src="{{URL::asset('public/js/entities/dishType.js')}}"></script>
+<script src="{{URL::asset('public/js/entities/identificationType.js')}}"></script>
