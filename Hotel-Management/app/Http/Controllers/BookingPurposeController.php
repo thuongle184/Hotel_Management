646f94d40cbe8f3cba13862d1b0ee7d1534b5e0b
Modern Coding <cormorant.programming@gmail.com>
2019-05-27 21:55:07 +0700

    Forbidden

--- Hotel-Management/app/Http/Controllers/BookingPurposeController.php
+++ Hotel-Management/app/Http/Controllers/BookingPurposeController.php
@@ -13,6 +13,11 @@ use Session;
 
 class BookingPurposeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
--- Hotel-Management/routes/web.php
+++ Hotel-Management/routes/web.php
@@ -40,3 +40,7 @@ Route::patch('dishes/{dish}/discard_picture', 'DishController@discardPicture')->
 Route::get('login', 'LoginController@login')->name('login');
 Route::post('login', 'LoginController@authenticate')->name('login.authenticate');
 Route::get('logout', 'LoginController@logout')->name('login.logout');
+
+Route::get('/forbidden', function () {
+  return view('forbidden');
+})->name('forbidden');
