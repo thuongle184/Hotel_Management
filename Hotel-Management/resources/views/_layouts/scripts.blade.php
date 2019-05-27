73b16c4e7c574cefc5cc5cbe3da28d55b423150b
nguyenhuutuan123 <tuan.nguyen.106902@gmail.com>
2019-05-27 23:46:08 +0700

    booking type has finished

--- Hotel-Management/resources/views/_layouts/scripts.blade.php
+++ Hotel-Management/resources/views/_layouts/scripts.blade.php
@@ -21,5 +21,6 @@
 <script src="{{URL::asset('js/entities/userType.js')}}"></script>
 <script src="{{URL::asset('js/entities/user.js')}}"></script>
 <script src="{{URL::asset('js/entities/company.js')}}"></script>
+<script src="{{URL::asset('js/entities/bookingType.js')}}"></script>
 
 
