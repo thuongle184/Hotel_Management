fd12981fc6c234c40ad392b28f74c5018e638c2b
ThiKiet <kiet.alang@student.passerellesnumeriques.org>
2019-05-30 09:18:46 +0700

    slides

---
+++ Hotel-Management/app/Http/Helpers/SlideHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+function mySlideHelperWelcomePage() {
+
+  return [
+    
+    [
+      'url'     =>  URL::asset('images/slides/slide-1.jpg'),
+      'title'   =>  'Our entrance',
+      'text'    =>  'Enjoy our cocktail next to our swimming-pool as a welcome gift'
+    ],
+
+    [
+      'url'     =>  URL::asset('images/slides/slide-2.jpg'),
+      'title'   =>  'Our massage service',
+      'text'    =>  'Enjoy our great massage service before or after your bath'
+    ],
+
+    [
+      'url'     =>  URL::asset('images/slides/slide-3.jpg'),
+      'title'   =>  'Our swimming-pool next to the beach',
+      'text'    =>  'The choice between salt and chlorine water at a hair distance'
+    ],
+
+    [
+      'url'     =>  URL::asset('images/slides/slide-4.jpg'),
+      'title'   =>  'Our olympic roof swimming-pool',
+      'text'    =>  'Compete with more than 100 guests!'
+    ],
+
+    [
+      'url'     =>  URL::asset('images/slides/slide-5.jpg'),
+      'title'   =>  'Our lounge',
+      'text'    =>  'The best place on Earth where to meet your business partners!'
+    ]
+
+  ];
+
+}
--- Hotel-Management/resources/views/welcome.blade.php
+++ Hotel-Management/resources/views/welcome.blade.php
@@ -2,8 +2,65 @@
 
 @section('content')
 
-  <div id="page-wrapper">
-    <h1>WELCOME!</h1>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      @foreach(mySlideHelperWelcomePage() as $slide)
+        
+        @if($loop->first)
+          <li 
+            data-target="#carouselExampleIndicators"
+            data-slide-to="{!! $loop->index !!}"
+            class="active"
+          ></li>
+        
+        @else
+          <li 
+            data-target="#carouselExampleIndicators"
+            data-slide-to="{!! $loop->index !!}"
+          ></li>
+        
+        @endif
+        
+      @endforeach
+    </ol>
+    
+    <div class="carousel-inner">
+      @foreach(mySlideHelperWelcomePage() as $slide)
+        
+        @if($loop->first)
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="{!! $slide['url'] !!}">
+            
+            <div class="carousel-caption d-none d-md-block bg-white text-dark">
+              <h5 class="font-weight-bold">{!! strtoupper($slide['title']) !!}</h5>
+              <p class="font-italic">{!! $slide['text'] !!}</p>
+            </div>
+          </div>
+        
+        @else
+          <div class="carousel-item">
+            <img class="d-block w-100" src="{!! $slide['url'] !!}">
+            
+            <div class="carousel-caption d-none d-md-block bg-white text-dark">
+              <h5 class="font-weight-bold">{!! strtoupper($slide['title']) !!}</h5>
+              <p class="font-italic">{!! $slide['text'] !!}</p>
+            </div>
+          </div>
+        
+        @endif
+
+      @endforeach
+    </div>
+    
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 
 @endsection
